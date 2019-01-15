4a54a060ffb42ad9d5351e671acb9231f3727260
Jordan Ayvazian <jaayvazian@gmail.com>
2019-01-15 15:54:25 -0800

    laptop config

---
+++ .config/fish/config.fish
@@ -0,0 +1,28 @@
+set -U fish_greeting
+kitty + complete setup fish | source
+
+# Actions
+alias t="task"
+alias ta="task add"
+alias tr="task done"
+alias tl="task list"
+alias cf="vim $HOME/.config/fish/config.fish"
+alias cb="vim $HOME/.config/bspwm/bspwmrc"
+alias cs="vim ~/.config/sxhkd/sxhkdrc"
+alias cpb="vim ~/.config/polybar/config"
+alias cpbl="vim ~/.config/polybar/launch.sh"
+alias g="git"
+alias gp="git push"
+alias gc="git commit"
+alias cv="vim ~/.vimrc"
+alias nf="neofetch --ascii_distro arch --gtk2 off --gtk3 off"
+alias autostart="sudo ln -s /usr/share/applications/$1.desktop /etc/xdg/autostart/"
+alias se="ls /usr/share/applications/ | grep $1"
+alias install="pikaur -S --noconfirm"
+# Directory Movement
+alias csca="$usb/School/CS140/Assignments"
+alias csja="$usb/School/CS105/Assignments"
+alias d="$HOME/Downloads"
+alias dc="$HOME/Documents"
+
+alias p="$HOME/Pictures"
