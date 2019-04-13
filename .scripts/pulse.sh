58b9abd4a6694841f91265fdd3682c7309597f10
Jordan Ayvazian <jaayvazian@gmail.com>
2019-04-12 21:35:00 -0700

    Had to rework this to make it work with the offical spotify client and not ncspot

--- .scripts/pulse.sh
+++ .scripts/pulse.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
-mod=10
-VOLUME=$(/home/jordan/pa_volume/pa_volume | grep -e 'ncspot' | grep -e '[0-9]*%' | sed "s/[^0-9]*//g" | xargs)
+id=
+devices=$(pactl list sink-inputs | grep -oP '(Sink Input #\K[^"]+)|(application.process.binary = "\K[^"]+)')
+IFS=' ' read -r -a array <<< "$(echo $devices)"
 
-if [ $1 == "lower" ]; then
-	NEWVOL=$(($VOLUME - $mod))
-elif [ $1 == "raise" ]; then
-  NEWVOL=$(($VOLUME + $mod))
-fi
+i=0
+for item in ${array[*]}; do
+  if [[ "$item" == "spotify" ]]; then
+    id=${array[$i-1]}
+    echo "$id"
+  fi
+  i=$(($i + 1))
+done
 
-if [[ $NEWVOL -gt 100 || "$1" == "max" ]]; then
-	NEWVOL=100
+if [[ $1 == "lower" ]]; then
+  pactl set-sink-input-volume $id -5%
+elif [[ $1 == "raise" ]]; then 
+  pactl set-sink-input-volume $id +5%
+elif [[ $NEWVOL -gt 100 || "$1" == "max" ]]; then
+  pactl set-sink-input-volume $id 100%
 elif [[ $NEWVOL -lt 0 || "$1" == "mute" ]]; then
-	NEWVOL=0
+	pactl set-sink-input-volume $id 0%
 fi
 
-echo $NEWVOL
-/home/jordan/pa_volume/pa_volume "ALSA plug-in [ncspot]" $NEWVOL
+
+
+
